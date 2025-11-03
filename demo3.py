0d90afd3e4dc2fc0926a1f8e693ce4a573938081
PAVITHRA636 <pavithra242005@gmail.com>
2025-11-03 14:39:01 +0530

    initial

---
+++ demo3.py
@@ -0,0 +1,6 @@
+#get input for variable mark.if mark>35 print pass.else print fail
+mark=int(input())
+if(mark>35):
+    print("Pass")
+else:
+    print("Fail")
---
+++ demo4.py
@@ -0,0 +1,6 @@
+num = int(input("Number: "))
+
+if (num % 3 == 0 or num % 5 == 0):
+    print("Number is divisible by 3 or 5")
+else:
+    print("Number is not divisible by 3 or 5")
